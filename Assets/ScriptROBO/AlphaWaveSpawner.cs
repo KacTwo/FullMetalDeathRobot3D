ae7240ee90817d692cffaafb5d37bc21c5cd77b6
Fa3r <remigusz.starzak@gmail.com>
2022-05-23 11:49:27 +0200

    Update AlphaWaveSpawner.cs

--- Assets/ScriptROBO/AlphaWaveSpawner.cs
+++ Assets/ScriptROBO/AlphaWaveSpawner.cs
@@ -34,7 +34,7 @@ public class AlphaWaveSpawner : MonoBehaviour
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f,6f), 0 ), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
 
     }
