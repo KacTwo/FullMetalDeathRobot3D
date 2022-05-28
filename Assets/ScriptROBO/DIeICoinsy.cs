1ae7229cdfffe3efa98f4e499c13b25420e87273
Kac2 <92221954+KacTwo@users.noreply.github.com>
2022-05-28 21:00:04 +0200

    są errory ale działa

--- Assets/ScriptROBO/DIeICoinsy.cs
+++ Assets/ScriptROBO/DIeICoinsy.cs
@@ -6,19 +6,32 @@ public class DIeICoinsy : MonoBehaviour
 {
     [SerializeField] float coinValue = 100;
     [SerializeField] float health = 1;
+    [SerializeField] float deathtime = 2f;
+    [SerializeField] public ParticleSystem deathParticle;
+    [SerializeField] public AudioClip deathSound;
+    //[SerializeField] public AnimationClip deathAnimation;
+    //[SerializeField] public Animation anim;
+    // animacje nie dizalaja, nie mam sily walczyc z nimi 
+    void Start()
+    {
+       
+    }
     public void TakeDamage(float amount)
     {
         health -= amount;
         if (health <= 0f)
         {
             GameObject.Find("InGameManager").GetComponent<GameManager>().pointScore += coinValue;
-            Die();
+            Die(); //dodaje scora
         }
     }
     void Die()
     {
+        deathParticle.Play();
+        SoundMenager.instance.Play(deathSound);
+        //anim.Play("deathAnimation");//animacje
         Debug.Log("umar");
-        Destroy(gameObject);
+        Destroy(gameObject, deathtime);
     }
 
 }
