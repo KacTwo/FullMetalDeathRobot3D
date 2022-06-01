7ccf50547d505ee45640d85ed418b56f22b3c51a
Kac2 <92221954+KacTwo@users.noreply.github.com>
2022-06-01 03:59:33 +0200

    update
    po śmierci ładuje scene sklep, naprawiony crash związany z militia

--- Assets/ScriptROBO/Health/PlayerHealth.cs
+++ Assets/ScriptROBO/Health/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -21,7 +22,10 @@ public class PlayerHealth : MonoBehaviour
 
     void update()
     {
-       
+       if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene("Sklep1");
+                }
     }
 
 
